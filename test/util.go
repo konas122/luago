03e880d1036d5a67228a9c26abe47429477f32be
konas122 <2407626470@qq.com>
2025-04-18 14:32:31 +0800

    add `luac_insight` in `test.util`

--- test/util.go
+++ test/util.go
@@ -2,19 +2,178 @@ package test
 
 import (
 	"fmt"
-	. "luago/api"
+	"io/ioutil"
+	"luago/api"
+	"luago/chunk"
+	"luago/state"
+	"luago/vm"
 )
 
-func printStack(ls LuaState) {
+func LuacInsight(filename string) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	proto := chunk.Undump(data)
+	list(proto)
+
+	print("\n=========================\n\n")
+
+	luaMain(proto)
+}
+
+func luaMain(proto *chunk.Prototype) {
+	nRegs := int(proto.MaxStackSize)
+	lstate := state.New(nRegs+8, proto)
+	lstate.SetTop(nRegs)
+	for {
+		pc := lstate.PC()
+		inst := vm.Instruction(lstate.Fetch())
+		if inst.Opcode() != vm.OP_RETURN {
+			inst.Execute(lstate)
+			fmt.Printf("[%02d] %s", pc+1, inst.OpName())
+			printStack(lstate)
+		} else {
+			break
+		}
+	}
+}
+
+func list(f *chunk.Prototype) {
+	printHeader(f)
+	printCode(f)
+	printDetail(f)
+	for _, p := range f.Protos {
+		list(p)
+	}
+}
+
+func printHeader(f *chunk.Prototype) {
+	funcType := "main"
+	if f.LineDefined > 0 {
+		funcType = "function"
+	}
+
+	varargFlag := ""
+	if f.IsVararg > 0 {
+		varargFlag = "+"
+	}
+
+	fmt.Printf("\n%s <%s:%d,%d> (%d instructions)\n",
+		funcType, f.Source, f.LineDefined, f.LastLineDefined, len(f.Code))
+
+	fmt.Printf("%d%s params, %d slots, %d upvalues, ",
+		f.NumParams, varargFlag, f.MaxStackSize, len(f.Upvalues))
+
+	fmt.Printf("%d locals, %d constants, %d functions\n",
+		len(f.LocVars), len(f.Constants), len(f.Protos))
+}
+
+func printCode(f *chunk.Prototype) {
+	for pc, c := range f.Code {
+		line := "-"
+		if len(f.LineInfo) > 0 {
+			line = fmt.Sprintf("%d", f.LineInfo[pc])
+		}
+		i := vm.Instruction(c)
+		fmt.Printf("\t%d\t[%s]\t%s \t", pc+1, line, i.OpName())
+		printOperands(i)
+		fmt.Printf("\n")
+	}
+}
+
+func printDetail(f *chunk.Prototype) {
+	fmt.Printf("constants (%d):\n", len(f.Constants))
+	for i, k := range f.Constants {
+		fmt.Printf("\t%d\t%s\n", i+1, constantToString(k))
+	}
+
+	fmt.Printf("locals (%d):\n", len(f.LocVars))
+	for i, locVar := range f.LocVars {
+		fmt.Printf("\t%d\t%s\t%d\t%d\n",
+			i, locVar.VarName, locVar.StartPC+1, locVar.EndPC+1)
+	}
+
+	fmt.Printf("upvalues (%d):\n", len(f.Upvalues))
+	for i, upval := range f.Upvalues {
+		fmt.Printf("\t%d\t%s\t%d\t%d\n",
+			i, upvalName(f, i), upval.Instack, upval.Idx)
+	}
+}
+
+func constantToString(k interface{}) string {
+	switch k.(type) {
+	case nil:
+		return "nil"
+	case bool:
+		return fmt.Sprintf("%t", k)
+	case float64:
+		return fmt.Sprintf("%g", k)
+	case int64:
+		return fmt.Sprintf("%d", k)
+	case string:
+		return fmt.Sprintf("%q", k)
+	default:
+		return "?"
+	}
+}
+
+func upvalName(f *chunk.Prototype, idx int) string {
+	if len(f.UpvalueNames) > 0 {
+		return f.UpvalueNames[idx]
+	}
+	return "-"
+}
+
+func printOperands(i vm.Instruction) {
+	switch i.OpMode() {
+	case vm.IABC:
+		a, b, c := i.ABC()
+
+		fmt.Printf("%d", a)
+		if i.BMode() != vm.OpArgN {
+			if b > 0xFF {
+				fmt.Printf(" %d", -1-b&0xFF)
+			} else {
+				fmt.Printf(" %d", b)
+			}
+		}
+		if i.CMode() != vm.OpArgN {
+			if c > 0xFF {
+				fmt.Printf(" %d", -1-c&0xFF)
+			} else {
+				fmt.Printf(" %d", c)
+			}
+		}
+	case vm.IABx:
+		a, bx := i.ABx()
+
+		fmt.Printf("%d", a)
+		if i.BMode() == vm.OpArgK {
+			fmt.Printf(" %d", -1-bx)
+		} else if i.BMode() == vm.OpArgU {
+			fmt.Printf(" %d", bx)
+		}
+	case vm.IAsBx:
+		a, sbx := i.AsBx()
+		fmt.Printf("%d %d", a, sbx)
+	case vm.IAx:
+		ax := i.Ax()
+		fmt.Printf("%d", -1-ax)
+	}
+}
+
+func printStack(ls api.LuaState) {
 	top := ls.GetTop()
 	for i := 1; i <= top; i++ {
 		t := ls.Type(i)
 		switch t {
-		case LUA_TBOOLEAN:
+		case api.LUA_TBOOLEAN:
 			fmt.Printf("[%t]", ls.ToBoolean(i))
-		case LUA_TNUMBER:
+		case api.LUA_TNUMBER:
 			fmt.Printf("[%g]", ls.ToNumber(i))
-		case LUA_TSTRING:
+		case api.LUA_TSTRING:
 			fmt.Printf("[%q]", ls.ToString(i))
 		default: // other values
 			fmt.Printf("[%s]", ls.TypeName(t))
